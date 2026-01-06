f3276917392da48a1ec2aa95c26fdaa55a0760d8
yanpe <leonardofonsecadmc@gmail.com>
2026-01-06 20:36:07 -0300

    Add files via upload

---
+++ identifica_humor.py
@@ -0,0 +1,10 @@
+a = int(input("Escolha um número 1, 2 ou 3: "))
+
+if a == 1:
+    print("Você está triste 😢")
+elif a == 2:
+    print("Você está normal 🙂")
+elif a == 3:
+    print("Você está feliz 😁")
+else:
+    print("Número inválido ❌")
